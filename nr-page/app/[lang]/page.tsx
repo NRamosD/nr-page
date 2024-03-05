a7d334a460c2d4a7dc2eb3ee9463a230c67a46b9
NixonR <nixonramosdue@gmail.com>
2024-03-05 01:07:01 -0500

    ready summary

--- nr-page/app/[lang]/page.tsx
+++ nr-page/app/[lang]/page.tsx
@@ -1,7 +1,11 @@
 "use client"
 import ecuador from "@/public/assets/img/banderaecuador.svg"
 import externallink from "@/public/assets/img/externallinksvgrepo.svg"
-
+import freecodecamp from "@/public/assets/img/freecodecamp.svg"
+import google from "@/public/assets/img/google.png"
+import meta from "@/public/assets/img/meta.png"
+import harvard from "@/public/assets/img/hardvard.png"
+import georgiatech from "@/public/assets/img/gt-logo.svg"
 
 import Image from 'next/image'
 import { Locale, getDictionary } from './dictionaries.ts'
@@ -44,7 +48,9 @@ export default async function Home({ params: { lang } }: Props) {
           </div> */}
         </div>
       </div>
-      <div id='summary' className={`h-[100vh] w-[90vw] md:w-[80vw] xl:w-[60vw] mb-20 bg-green-500`}>
+
+
+      <div id='summary' className={`h-[100vh] w-[90vw] md:w-[80vw] xl:w-[60vw] mb-20`}>
         <div className='grid grid-cols-1 gap-5'>
           <div className='w-full grid grid-cols-6 text-[30px] md:text-[50px] '>
             <div className='my-auto grid gap-y-5 col-span-2 '>
@@ -61,14 +67,16 @@ export default async function Home({ params: { lang } }: Props) {
                     <span>24</span>
                 </div>
             </div>
-            <div className='m-auto grid gap-y-5 col-span-4 '>
-              <div className='grid  text-[20px] md:text-[40px]'>
+            <div className='my-auto grid gap-y-5 col-span-4 '>
+              <div className=' grid text-[20px] md:text-[35px]'>
                 <span>
                   {intl.summary.istudied}
                 </span>
                 <div className="flex">
-                  <p className="text-[15px] md:text-[30px]">{intl.summary.university}</p>
-                  <Link className="w-fit m-auto" href={"https://www.espoch.edu.ec"}>
+                  <p className="text-[15px] md:text-[26px] gradient-text-university">
+                    {intl.summary.university}
+                  </p>
+                  <Link target="_blank" className="w-fit my-auto rounded-md bg-white ml-2" href={"https://www.espoch.edu.ec"}>
                     <Image src={externallink}  alt={intl.summary.university} 
                     width={20} height={20}/>
                   </Link>
@@ -84,61 +92,87 @@ export default async function Home({ params: { lang } }: Props) {
           </div>
           <div className='w-full grid grid-cols-6 gap-5 text-[30px] md:text-[50px]'>
             <div className="col-span-6 text-left">
-              <h3 className="text-[20px] md:text-[30px]">{intl.summary.coursesandcertifications}</h3>
+              <h3 className="text-[20px] md:text-[30px] underline">{intl.summary.coursesandcertifications}</h3>
             </div>
-            <div className="grid gap-2 col-span-3 bg-black p-2 text-[15px] md:text-[20px]">
-              <div className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
+            <div className="grid gap-2 col-span-3 p-2 text-[14px] md:text-[18px]">
+              <Link target="_blank" href={"https://www.coursera.org/account/accomplishments/specialization/8UKZ8DUMEAAB"} className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
                 <h3>
-                  kjelkwqjelkjqwlek
+                  {intl.summary.specializationfrontmeta}
                 </h3>
-                <Link className="w-fit m-auto" href={"https://www.espoch.edu.ec"}>
-                    <Image src={externallink}  alt={intl.summary.university} 
-                    width={20} height={20}/>
-                  </Link>
-              </div>
-              <div className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
+                <Image className="" src={externallink}  alt={intl.summary.university} 
+                  width={20} height={20}/>
+              </Link>
+              
+              <Link target="_blank" href={"https://certificates.cs50.io/64eec961-4a0a-473d-a62d-9860373864ab.pdf?size=letter"} className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
                 <h3>
-                  kjelkwqjelkjqwlek
+                  {intl.summary.harvardcs50py}
                 </h3>
-                <Link className="w-fit m-auto" href={"https://www.espoch.edu.ec"}>
-                    <Image src={externallink}  alt={intl.summary.university} 
-                    width={20} height={20}/>
-                  </Link>
-              </div>
-              <div className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
+                <Image className="" src={externallink}  alt={intl.summary.university} 
+                  width={20} height={20}/>
+              </Link>
+              <Link target="_blank" href={"https://www.coursera.org/account/accomplishments/records/H9JBHSZVA4HB"} className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
                 <h3>
-                  kjelkwqjelkj wqekjqwklejqlwj elkqwa sdjlqwlek
+                  {intl.summary.csharpnet}
                 </h3>
-                <Link className="w-fit m-auto" href={"https://www.espoch.edu.ec"}>
-                    <Image src={externallink}  alt={intl.summary.university} 
-                    width={20} height={20}/>
-                  </Link>
-              </div>
-              <div className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
+                <Image className="" src={externallink}  alt={intl.summary.university} 
+                  width={20} height={20}/>
+              </Link>
+              <Link target="_blank" href={"https://www.coursera.org/account/accomplishments/verify/LNZDHVP6DS3A"} className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
                 <h3>
-                  kjelkwqjelkjqwlek
+                  {intl.summary.angularrouting}
                 </h3>
-                <Link className="w-fit m-auto" href={"https://www.espoch.edu.ec"}>
-                    <Image src={externallink}  alt={intl.summary.university} 
-                    width={20} height={20}/>
-                  </Link>
-              </div>
-              <div className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
+                <Image className="" src={externallink}  alt={intl.summary.university} 
+                  width={20} height={20}/>
+              </Link>
+              <Link target="_blank" href={"https://www.linkedin.com/in/nix-ramos/details/certifications/"} className="flex gap-2 transition-all duration-500 ease-out px-4 py-2 shadow-xl text-black bg-white rounded-md hover:translate-x-2 hover:bg-slate-300">
                 <h3>
-                  kjelkwqjelkjqwlek
+                  {intl.summary.viewall}
                 </h3>
-                <Link className="w-fit m-auto" href={"https://www.espoch.edu.ec"}>
-                    <Image src={externallink}  alt={intl.summary.university} 
-                    width={20} height={20}/>
-                  </Link>
-              </div>
+                <Image className="" src={externallink}  alt={intl.summary.university} 
+                  width={20} height={20}/>
+              </Link>
             </div>
-            <div className="col-span-3 bg-black p-2">
-wer
+            <div className="col-span-3 grid grid-cols-3 p-2">
+              <div>
+                <Image src={freecodecamp}
+                alt="freecodecamp"
+                width={100} height={50}
+                />
+              </div>
+              <div className=" ">
+                <Image src={google}
+                alt="google"
+                width={30} height={50}
+                className=""
+                />
+              </div>
+              <div className=" bg-white w-fit h-fit p-2">
+                <Image src={harvard}
+                alt="harvard"
+                width={100} height={50}
+                className=""
+                />
+              </div>
+              <div className=" bg-white w-fit h-fit">
+                <Image src={meta}
+                alt="meta"
+                width={100} height={50}
+                className=""
+                />
+              </div>
+              <div>
+                <Image src={georgiatech}
+                alt="georgiatech"
+                width={100} height={50}
+                className=""
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+
       <div id='skills' className={`h-[100vh] w-[90vw] md:w-[80vw] xl:w-[60vw] mb-20 bg-green-500`}>
         <div className='h-full grid grid-cols-10 grid-rows-7 md:grid-rows-1 gap-5 '>
           <div className='h-full w-full row-span-6 md:row-span-1 col-span-10 md:col-span-2 bg-yellow-300'>di</div>
