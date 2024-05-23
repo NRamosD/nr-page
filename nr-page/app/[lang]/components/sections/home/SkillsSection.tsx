b11a5e7ab89e18a3b9cb567e84f89b18005098bf
Nixon <nixonramosdue@gmail.com>
2024-05-23 17:49:57 -0500

    weno ya esta el frontend y se ve bien

--- nr-page/app/[lang]/components/sections/home/SkillsSection.tsx
+++ nr-page/app/[lang]/components/sections/home/SkillsSection.tsx
@@ -8,6 +8,18 @@ import 'aos/dist/aos.css';
 
 
 import ToolTip from '../../ToolTip';
+import html_logo from "../../../../../public/assets/img/techs/front/html.png"
+import css_logo from "../../../../../public/assets/img/techs/front/css.png"
+import js_logo from "../../../../../public/assets/img/techs/front/js.png"
+import ts_logo from "../../../../../public/assets/img/techs/front/typescript.png"
+import react_logo from "../../../../../public/assets/img/techs/front/react.png"
+import angular_logo from "../../../../../public/assets/img/techs/front/angular.png"
+import bootstrap_logo from "../../../../../public/assets/img/techs/front/bootstrap.png"
+import tailwindcss_logo from "../../../../../public/assets/img/techs/front/tailwindcss.png"
+import reactnative_logo from "../../../../../public/assets/img/techs/front/react_native.png"
+import nextjs_logo from "../../../../../public/assets/img/techs/front/nextjs-large.png"
+import android_logo from "../../../../../public/assets/img/techs/front/android_studio.png"
+import PulseLoading from '../../PulseLoading';
 
 
 
@@ -49,9 +61,57 @@ export const SkillsSection = (props : {skills:any}) => {
             <h2 className="text-4xl xl:text-[5em] font-extrabold tracking-[.25em] xl:tracking-[1.5em] 2xl:text-center 2xl:ml-20 py-10">SKILLS</h2>
             <div className='grid xl:grid-cols-4 gap-10 w-[80vw] xl:w-[60vw] h-[700px] xl:h-[600px] rounded-md'>
                 <div className="h-full xl:col-span-2 ">
-                    <div className="h-[16rem] xl:h-[40rem] bg-gradient-to-b from-[#081a25] to-transparent rounded-md">
-                        qweq
-
+                    <div className="h-[16rem] xl:h-[40rem] bg-gradient-to-b from-[#081a25] to-transparent rounded-md grid grid-rows-2">
+                        <div className=' grid grid-cols-2'>
+                            <div className='m-auto'>
+                                <ToolTip text='holas xD'>
+                                    <Image src={js_logo} alt="react_logo" width={230} height={250}/>
+                                </ToolTip>
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={ts_logo} alt="react_logo" width={200}/>
+                            </div>
+                        </div>
+                        <div className=' grid grid-cols-6 gap-2'>
+                            <div className='m-auto'>
+                                <Image src={html_logo} alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={css_logo} alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={bootstrap_logo} alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={tailwindcss_logo} className='invert' alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <PulseLoading/>
+                                {/* <Image src={react_logo} alt="react_logo" /> */}
+                            </div>
+                            <div className='m-auto'>
+                                {/* <Image src={angular_logo} alt="react_logo" /> */}
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={react_logo} alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={angular_logo} alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={nextjs_logo} className='invert' alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={reactnative_logo} alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <Image src={android_logo} alt="react_logo" />
+                            </div>
+                            <div className='m-auto'>
+                                <PulseLoading/>
+                                {/* <Image src={angular_logo} alt="react_logo" /> */}
+                            </div>
+                        </div>
                     </div>
                     <div className="h-20 w-full flex">
                         <h2 className=" text-[1.9em] text-center m-auto">
